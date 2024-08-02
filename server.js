a6ad9d9a2b29f26e4279416ddb2b720ed82ba42e
YadamVinay369 <vinay.y21@iiits.in>
2024-08-02 23:50:22 +0530

    connection between MongoDB atlas and server is established successfully

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require('express')
+const colors = require('colors')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require("dotenv")
+const connectDB = require('./config/db')
+
+//dot config (If .env present in the config folder)
+// dotenv.config(path: './config/')
+
+//dot config (Here .env is present in root itself)
+dotenv.config()
+
+//mongoDB connection
+connectDB();
+
+//rest object
 const app = express()
-const PORT = 3000
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+
+//port
+const PORT = process.env.PORT || 8000;
 
 
 app.listen(PORT,()=>{
-    console.log("server is running on",PORT)
+    console.log(`Server is Running in ${process.env.DEV_MODE} mode on Port ${process.env.PORT}`.bgBlue.white)
 })
