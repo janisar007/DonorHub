4f2b05ad4124378031d6070eaf3188424cfc8225
YadamVinay369 <vinay.y21@iiits.in>
2024-08-10 21:14:03 +0530

    feat: User Registration feature added with password encryption

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-const express = require('express')
-const colors = require('colors')
-const cors = require('cors')
-const morgan = require('morgan')
-const dotenv = require("dotenv")
-const connectDB = require('./config/db')
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //dot config (If .env present in the config folder)
 // dotenv.config(path: './config/')
 
 //dot config (Here .env is present in root itself)
-dotenv.config()
+dotenv.config();
 
 //mongoDB connection
 connectDB();
 
 //rest object
-const app = express()
+const app = express();
 
 //middlewares
-app.use(express.json())
-app.use(cors())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
+app.use("/api/v1/auth", require("./routes/authRoutes"));
 
 //port
 const PORT = process.env.PORT || 8000;
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is Running in ${process.env.DEV_MODE} mode on Port ${process.env.PORT}`.bgBlue.white)
-})
+app.listen(PORT, () => {
+  console.log(
+    `Server is Running in ${process.env.DEV_MODE} mode on Port ${process.env.PORT}`
+      .bgBlue.white
+  );
+});
