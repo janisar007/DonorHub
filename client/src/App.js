c8b3baf5b6a48c0d613f781382c2ba68fd4009c8
YadamVinay369 <vinay.y21@iiits.in>
2024-08-16 17:08:43 +0530

    feat: updated routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-danger">DonorHub</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
