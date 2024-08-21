3735dcec2faa584ad947bf86e7a74ed2a0084053
YadamVinay369 <vinay.y21@iiits.in>
2024-08-21 23:14:57 +0530

    feat: spinner for loading state is added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Spinner from "../components/shared/Spinner";
+import { toast } from "react-toastify";
 
 const Homepage = () => {
-  return <>Home page</>;
+  const { loading, error } = useSelector((state) => state.auth);
+  return (
+    <>
+      {error && (
+        <span style={{ display: "none" }}>{toast.error(error.payload)}</span>
+      )}
+      {loading ? <Spinner /> : <h1>Home page</h1>}
+    </>
+  );
 };
 
 export default Homepage;
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import Form from "../../components/shared/Form/Form";
+import { useSelector } from "react-redux";
+import Spinner from "../../components/shared/Spinner";
+import { toast } from "react-toastify";
 
 const Regsiter = () => {
+  const { loading, error } = useSelector((state) => state.auth);
   return (
     <>
-      <div className="row g-0">
-        <div className="col-md-8 form-banner">
-          <img src="./assets/images/banner2.jpg" alt="RegisterImage" />
+      {error && (
+        <span style={{ display: "none" }}>{toast.error(error.payload)}</span>
+      )}
+
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="row g-0">
+          <div className="col-md-8 form-banner">
+            <img src="./assets/images/banner2.jpg" alt="RegisterImage" />
+          </div>
+          <div className="col-md-4 form-container">
+            <Form
+              formTitle={"Register"}
+              submitBtn={"Register"}
+              formType={"register"}
+            />
+          </div>
         </div>
-        <div className="col-md-4 form-container">
-          <Form
-            formTitle={"Register"}
-            submitBtn={"Register"}
-            formType={"register"}
-          />
-        </div>
-      </div>
+      )}
     </>
   );
 };
