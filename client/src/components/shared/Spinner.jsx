4adda273847dc545ae97c1a8f2a4ef8d51aefb40
YadamVinay369 <vinay.y21@iiits.in>
2024-08-21 23:15:34 +0530

    feat: spinner created

---
+++ client/src/components/shared/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="container">
+      <div className="dash uno" />
+      <div className="dash dos" />
+      <div className="dash tres" />
+      <div className="dash cuatro" />
+    </div>
+  );
+};
+
+export default Spinner;
