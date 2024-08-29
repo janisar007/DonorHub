3f86cd838dad83aa643fa3967e73a629f2d913d8
YadamVinay369 <vinay.y21@iiits.in>
2024-08-29 22:42:55 +0530

    refactor: spinner updated

--- client/src/components/shared/Spinner.jsx
+++ client/src/components/shared/Spinner.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 const Spinner = () => {
-  return (
-    <div className="container">
-      <div className="dash uno" />
-      <div className="dash dos" />
-      <div className="dash tres" />
-      <div className="dash cuatro" />
-    </div>
-  );
+  return <div id="loader"></div>;
 };
 
 export default Spinner;
