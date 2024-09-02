f34ebc332da8b0dad696520437a71b0edd57cbab
YadamVinay369 <vinay.y21@iiits.in>
2024-09-02 09:56:21 +0530

    feat: add inventory 'blood-in' feature added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Spinner from "../components/shared/Spinner";
 import { toast } from "react-toastify";
 import Layout from "../components/shared/Layout/Layout";
+import Modal from "../components/shared/Modal/Modal";
+import API from "../services/API";
+import moment from "moment";
 
 const Homepage = () => {
   const { loading, error } = useSelector((state) => state.auth);
+  const [data, setData] = useState([]);
+
+  const getBloodRecords = async () => {
+    try {
+      const { data } = await API.get("/inventory/get-inventory");
+      if (data?.success) {
+        setData(data?.inventory);
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBloodRecords();
+  }, []);
   return (
     <Layout>
       {error && (
         <span style={{ display: "none" }}>{toast.error(error.payload)}</span>
       )}
-      {loading ? <Spinner /> : <h1>Home page</h1>}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="container">
+            <h4
+              className="ms-4"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+              style={{ cursor: "pointer" }}
+            >
+              <i className="fa-solid fa-plus text-success py-4" />
+              Add Inventory
+            </h4>
+
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Blood Group</th>
+                  <th scope="col">Inventory Type</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Donar Email</th>
+                  <th scope="col">Date & Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data?.map((record) => (
+                  <tr key={record._id}>
+                    <td>{record.bloodGroup}</td>
+                    <td>{record.inventoryType}</td>
+                    <td>{record.quantity}</td>
+                    <td>{record.donarEmail}</td>
+                    <td>
+                      {moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <Modal />
+          </div>
+        </>
+      )}
     </Layout>
   );
 };
