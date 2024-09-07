45ad9034b8ae0afd154a80b51fe2d2f0b092292c
YadamVinay369 <vinay.y21@iiits.in>
2024-09-07 23:41:28 +0530

    feat: logic updated to accept donations for donar dashboard and consumer for hospital dashboard

--- client/src/components/shared/Layout/Sidebar.jsx
+++ client/src/components/shared/Layout/Sidebar.jsx
@@ -1,15 +1,78 @@
 import React from "react";
-import { userMenu } from "./Menus/userMenu";
 import { useLocation, Link } from "react-router-dom";
 import "../../../styles/Layout.css";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
   const location = useLocation();
+  const { user } = useSelector((state) => state.auth);
   return (
     <div>
       <div className="sidebar">
         <div className="menu">
-          {userMenu.map((menu) => {
+          {user?.role === "organisation" && (
+            <>
+              <div
+                className={`menu-item ${location.pathname === "/" && "active"}`}
+              >
+                <i className="fa-solid fa-warehouse"></i>
+                <Link to="/">Inventory</Link>
+              </div>
+
+              <div
+                className={`menu-item ${
+                  location.pathname === "/donar" && "active"
+                }`}
+              >
+                <i className="fa-solid fa-hand-holding-medical"></i>
+                <Link to="/donar">Donar</Link>
+              </div>
+
+              <div
+                className={`menu-item ${
+                  location.pathname === "/hospital" && "active"
+                }`}
+              >
+                <i className="fa-solid fa-hospital"></i>
+                <Link to="/hospital">Hospital</Link>
+              </div>
+            </>
+          )}
+
+          {(user?.role === "donar" || user?.role === "hospital") && (
+            <div
+              className={`menu-item ${
+                location.pathname === "/organisation" && "active"
+              }`}
+            >
+              <i className="fa-solid fa-building-ngo"></i>
+              <Link to="/organisation">Organisation</Link>
+            </div>
+          )}
+
+          {user?.role === "hospital" && (
+            <div
+              className={`menu-item ${
+                location.pathname === "/consumer" && "active"
+              }`}
+            >
+              <i className="fa-solid fa-republican"></i>
+              <Link to="/consumer">Consumer</Link>
+            </div>
+          )}
+
+          {user?.role === "donar" && (
+            <div
+              className={`menu-item ${
+                location.pathname === "/donation" && "active"
+              }`}
+            >
+              <i className="fa-solid fa-hand-holding-medical fa-flip-horizontal"></i>
+              <Link to="/donation">Donations</Link>
+            </div>
+          )}
+
+          {/* {userMenu.map((menu) => {
             const isActive = location.pathname === menu.path;
             return (
               <div
@@ -20,7 +83,7 @@ const Sidebar = () => {
                 <Link to={menu.path}>{menu.name}</Link>
               </div>
             );
-          })}
+          })} */}
         </div>
       </div>
     </div>
