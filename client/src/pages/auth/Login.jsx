280a28b7f189d9a50e72edd5f2b0ee3453a5376f
YadamVinay369 <vinay.y21@iiits.in>
2024-09-08 15:38:04 +0530

    styles: images updated for login and register pages

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -16,11 +16,11 @@ const Login = () => {
         <Spinner />
       ) : (
         <div className="row g-0">
-          <div className="col-md-8 form-banner">
-            <img src="./assets/images/banner1.jpg" alt="loginImage" />
-          </div>
           <div className="col-md-4 form-container">
             <Form formTitle={"Login"} submitBtn={"Login"} formType={"login"} />
+          </div>
+          <div className="col-md-8 form-banner">
+            <img src="./assets/images/login.jpg" alt="loginImage" />
           </div>
         </div>
       )}
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -16,15 +16,15 @@ const Register = () => {
         <Spinner />
       ) : (
         <div className="row g-0">
-          <div className="col-md-8 form-banner">
-            <img src="./assets/images/banner2.jpg" alt="RegisterImage" />
-          </div>
           <div className="col-md-4 form-container">
             <Form
               formTitle={"Register"}
               submitBtn={"Register"}
               formType={"register"}
             />
+          </div>
+          <div className="col-md-8 form-banner">
+            <img src="./assets/images/new.jpg" alt="RegisterImage" />
           </div>
         </div>
       )}
